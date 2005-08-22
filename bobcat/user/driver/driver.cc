3ed2bc4fed3aeba9c94d358c583e74e21e13948e
Frank B. Brokken <f.b.brokken@rug.nl>
2005-08-22 14:03:41 +0000

    Added `class User'
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@7 95c995b8-3b1c-0410-acf1-adacd5d40d98

---
+++ bobcat/user/driver/driver.cc
@@ -0,0 +1,21 @@
+#include <bobcat/user>
+#include <iostream>
+
+using namespace std;
+using namespace FBB;
+
+int main()
+{
+    User  user;
+
+    cout << "\n"
+        "name       : " << user.name()      << "\n" <<
+        "password   : " << user.password()  << "\n" <<
+        "user id    : " << user.userid()    << "\n" <<
+        "group id   : " << user.groupid()   << "\n" <<
+        "real name  : " << user.realname()  << "\n" <<
+        "home dir   : " << user.homedir()   << "\n" <<
+        "shell      : " << user.shell()     << "\n" << endl;
+
+    return 0;
+}
