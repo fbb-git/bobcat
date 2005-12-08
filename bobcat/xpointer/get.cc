8e352807fe5cf27bf0850370cc70c432945c3b40
Frank B. Brokken <f.b.brokken@rug.nl>
2005-12-08 15:05:54 +0000

    Added xpointer
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@58 95c995b8-3b1c-0410-acf1-adacd5d40d98

---
+++ bobcat/xpointer/get.cc
@@ -0,0 +1,16 @@
+#include "xpointer.ih"
+
+bool Xpointer::get(int *x, int *y) const
+{
+    Window rootret;
+    Window child;
+    Window root = DefaultRootWindow(s_theDisplay);
+    unsigned keys;
+    int dum_x;
+    int dum_y;
+
+    return 
+        XQueryPointer(s_theDisplay, root, &rootret, &child,
+                        x, y, &dum_x, &dum_y, &keys);
+}
+
