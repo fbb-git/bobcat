9079545216b78cd732267e68cb148373b6e77319
Frank B. Brokken <f.b.brokken@rug.nl>
2005-12-17 16:15:39 +0000

    Up to the 1.5.0a release
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@69 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/process/stop.cc
+++ bobcat/process/stop.cc
@@ -2,15 +2,26 @@
 
 int Process::stop()
 {
+    if (!getPid())                  // pid = 0 occurs when a Process 
+        return 0;                   // object was created, but did not yet
+                                    // fork(), and its program terminates.
+                                    // This may happen, e.g., in a daemon.
     d_command.clear();
 
     d_child_inp.reset(0);
 
     if (pid_t pid = getPid())
-        kill(pid, SIGTERM) && kill(pid, SIGKILL);
+        kill(pid, SIGTERM);
+
+    if (pid_t pid = getPid())
+        kill(pid, SIGTERM);
+
+    if (pid_t pid = getPid())
+        kill(pid, SIGKILL);
 
     d_child_outp.reset(0);
     d_child_errp.reset(0);
 
     return d_ret = waitForChild();
 }
+
