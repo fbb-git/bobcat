5f22d94baf66015c75a7405cf19c50e033898ba3
Frank B. Brokken <f.b.brokken@rug.nl>
2006-07-18 21:02:24 +0000

    See deb's changelog
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@99 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/cmdfinderbase/usefirstcmd.cc
+++ bobcat/cmdfinderbase/usefirstcmd.cc
@@ -4,6 +4,8 @@ void CmdFinderBase::useFirstCmd(std::string const &cmd)
 {
     istringstream in(cmd);
 
+    d_beyond.erase();         // clear any old remaining `beyond' info
+
     in >> d_cmd >> d_beyond;
 
     string trailer;
