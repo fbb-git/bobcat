e41ce0ec999e66dbbe923bcd8d1f0806126ceb7e
George Danchev <danchev@spnet.net>
2006-07-27 23:17:11 +0300

    Imported Debian patch 1.10.0-5

--- string/unescape.cc
+++ string/unescape.cc
@@ -7,7 +7,7 @@ namespace
 
     unsigned handleOctal(String *dest, String const &src, unsigned pos)
     {
-        size_t const nOct = 3;                // need exactly 3 octals
+        unsigned const nOct = 3;                // need exactly 3 octals
 
         unsigned pos2 = min(pos + nOct, 
                             src.find_first_not_of("01234567", pos));
@@ -28,7 +28,7 @@ namespace
 
     unsigned handleHex(String *dest, String const &src, unsigned pos)
     {
-        size_t const nHex = 2;                // need exactly 2 hex digits
+        unsigned const nHex = 2;                // need exactly 2 hex digits
 
         ++pos;                                  // skip the 'x'
         unsigned pos2 = min(pos + nHex, 
