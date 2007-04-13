533f134e15522984c312bbb611a1668bf61e3592
Frank B. Brokken <f.b.brokken@rug.nl>
2007-04-13 10:26:51 +0000

    see the changelog for 1.14.2
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@176 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/serversocket/serversocket1.cc
+++ bobcat/serversocket/serversocket1.cc
@@ -4,6 +4,11 @@ ServerSocket::ServerSocket(size_t port)
 :
     SocketBase(port)        // uses INADDR_ANY -> current host.
 {
+    int val = 1;    
+                            // Make sure the socket is reusable upon restarts
+    if (setsockopt(socket(), SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int)))
+        throw Errno("ServerSocket::ServerSocket(port) setsockopt() failed");
+
     if (bind(socket(), sockaddrPtr(), size()) < 0)
         throw Errno("ServerSocket::ServerSocket(port)");
 }
