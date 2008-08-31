5821acb7f57420e8d90dfc5fe9d75cfe3f39f6ed
Frank B. Brokken <f.b.brokken@rug.nl>
2008-08-31 16:11:40 +0000

    Various repairs for 1.20.0, see the changelog
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@260 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/glob/glob1.cc
+++ bobcat/glob/glob1.cc
@@ -6,13 +6,15 @@ Glob::Glob(string const &pattern, int flags, Dots dots)
 {
     d_share->users = 1;
 
-    if 
-    (
-        glob(pattern.c_str(), flags, 0, &d_share->globStruct)
-        ||
-        flags & ~(ERR | MARK | NOSORT | NOESCAPE | PERIOD)
-    )
-        throw Errno("Glob: glob() failed or illegal Flag specified");
+    d_ok = 
+        (
+            glob(pattern.c_str(), flags, 0, &d_share->globStruct)
+            ||
+            flags & ~(ERR | MARK | NOSORT | NOESCAPE | PERIOD)
+        );
+
+    if (!d_ok)
+        return;
 
     if (dots == FIRST)
         stable_partition(mbegin(), mend(), 
