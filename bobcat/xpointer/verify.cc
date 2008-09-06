01bf2f3505baa526e46c1d85c306501115a5c499
Frank B. Brokken <f.b.brokken@rug.nl>
2008-09-06 09:06:44 +0000

    Xpointer::verify() no longer closes the display
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@294 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/xpointer/verify.cc
+++ bobcat/xpointer/verify.cc
@@ -4,8 +4,5 @@ void Xpointer::verify() const
 {
     if (!s_theDisplay)
         throw Errno(1, "Can't open the display");
-        
-    if (!--s_counter)               // No more Xpointer objects ?
-        XCloseDisplay(s_theDisplay);
 }
     
