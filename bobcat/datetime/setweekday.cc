9a841e013e453f92146ae0d8f7e6a9ed12fd0843
Frank B. Brokken <f.b.brokken@rug.nl>
2008-09-14 19:12:53 +0000

    1.20.1: cout-cleanup from DateTime
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@300 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/datetime/setweekday.cc
+++ bobcat/datetime/setweekday.cc
@@ -9,28 +9,24 @@ bool DateTime::setWeekday(WeekDay weekday, Relative where)
 
     int difference = static_cast<int>(weekday) - tmStruct.tm_wday;
 
-cout <<         tmStruct.tm_wday << ' ' <<  difference << endl;
-
     switch (where)
     {
         case NEXT:
             difference += 7;
         break;
+
         case LAST:
             difference  -= 7;
         break;
+
         case THIS_WEEK:
         break;
+
         default:
         throw Errno(1, "DateTime::setWeekday(): invalid Relative spec.");
     }
 
-cout <<         tmStruct.tm_mday << ' ' <<  difference << endl;
-
     tmStruct.tm_mday += difference;
-
-cout <<         tmStruct.tm_mday << ' ' <<  difference << endl;
-
     
     return updateTime(&tmStruct);
 }
