d6c973ceaf04aeb05ff03034fbbe85b3f6690ce6
Frank B. Brokken <f.b.brokken@rug.nl>
2008-09-26 07:48:23 +0000

    Repaired User returntypes in manpage, and other things; see TODO
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@302 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/process/driver/driver.cc
+++ bobcat/process/driver/driver.cc
@@ -8,27 +8,30 @@ using namespace FBB;
 int main()
 {
     string line;
+
                                         // setup a command
-    Process process("/bin/cat", Process::CIN |
+    Process process("/usr/bin/sha1sum -", Process::CIN |
                                 Process::COUT |
                                 Process::IGNORE_CERR);
 
     process.start();                    // start it
 
     process << "hello world" << endl;   // insert this text
+//    process.close();
+
     getline(process, line);             // read its `cat' output
     cout << line << endl;               // show it
 
-    process = "`/bin/ls`";              // stop the existing command,
-                                        // start another command
-                                        // (assignment implies
-                                        // `stop()')
-
-    while (getline(process, line))      // read all its output
-        cout << line << endl;
-
-    cout << process.stop() << endl;     // show the process' exit
-                                        // value
+//    process = "`/bin/ls`";              // stop the existing command,
+//                                        // start another command
+//                                        // (assignment implies
+//                                        // `stop()')
+//
+//    while (getline(process, line))      // read all its output
+//        cout << line << endl;
+//
+//    cout << process.stop() << endl;     // show the process' exit
+//                                        // value
     return 0;
 }
 
