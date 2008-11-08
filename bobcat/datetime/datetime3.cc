1e84eeace53529201036c21a14e01dba462b2310
Frank B. Brokken <f.b.brokken@rug.nl>
2008-11-08 21:47:07 +0000

    WIP
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@343 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/datetime/datetime3.cc
+++ bobcat/datetime/datetime3.cc
@@ -5,8 +5,8 @@ DateTime::DateTime(time_t time, TimeType type)
     d_type(type)
 {
     zoneCorrection();
-
     initializeTime(time, 0, 0);
+
     setDisplayZone(0);
     utcSec2timeStruct(&d_tm, d_time);
 }
