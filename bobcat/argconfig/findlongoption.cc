1bc3910541d2792efdac6ef23319d0bc444eba74
Frank B. Brokken <f.b.brokken@rug.nl>
2008-11-21 16:12:16 +0000

    textual and small bug repairs
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@367 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/argconfig/findlongoption.cc
+++ bobcat/argconfig/findlongoption.cc
@@ -4,5 +4,8 @@ ArgConfig::const_RE_iterator ArgConfig::findLongOption(int optChar) const
 {
     map<int, string>::const_iterator optIt = d_longOption.find(optChar);
 
-    return longConfigOpt(optIt->second);
+    return optIt == d_longOption.end() ? 
+                        endRE() 
+                    :
+                        longConfigOpt(optIt->second);
 }
