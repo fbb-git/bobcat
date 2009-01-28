e27ba3d7ffb9bc528539924227d72a9bf5e41957
Frank B. Brokken <f.b.brokken@rug.nl>
2009-01-28 10:53:14 +0000

    Working on man-pages of new classes
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@388 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/ofoldstreambuf/modifyindent.cc
+++ bobcat/ofoldstreambuf/modifyindent.cc
@@ -1,6 +1,6 @@
 #include "ofoldstreambuf.ih"
 
-void OFoldStreambuf::chgIndent(int delta)
+void OFoldStreambuf::modifyIndent(int delta)
 {
     d_indent += delta;
     if (static_cast<int>(d_indent) < 0)
