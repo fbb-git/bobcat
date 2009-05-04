9595415062a3f9e99e814f349a48607e978a33ec
Frank B. Brokken <f.b.brokken@rug.nl>
2009-05-04 19:15:20 +0000

    Additional repair to SocketBase: missing initializations
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@421 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/socketbase/socketbase2.cc
+++ bobcat/socketbase/socketbase2.cc
@@ -5,7 +5,11 @@ SocketBase::SocketBase(string const &host, uint16_t port)
     InetAddress(host, port)
 {
     d_sock = ::socket(AF_INET, SOCK_STREAM, 0);   // 0: protocol, should be 0
-
-    if (d_sock < 0)
-        d_msg = "SocketBase::SocketBase(host, port)";
+    d_msg = d_sock < 0 ? "SocketBase::SocketBase(host, port)" : 0;
 }
+
+
+
+
+
+
