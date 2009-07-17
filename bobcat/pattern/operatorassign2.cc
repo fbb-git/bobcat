b4288592daabffce039a7d11045d064ab88a86a9
Frank B. Brokken <f.b.brokken@rug.nl>
2009-07-17 12:16:20 +0000

    updated pattern
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@459 95c995b8-3b1c-0410-acf1-adacd5d40d98

---
+++ bobcat/pattern/operatorassign2.cc
@@ -0,0 +1,18 @@
+#include "pattern.ih"
+
+Pattern &Pattern::operator=(Pattern &&tmp)
+{
+    destroy();
+
+    d_regex = tmp.d_regex;
+    d_subExpression = tmp.d_subExpression;
+    d_nSub = tmp.d_nSub;
+    d_beyondLast = tmp.d_beyondLast;
+    d_text = tmp.d_text;
+    d_matchOptions = tmp.d_matchOptions;
+
+    tmp.d_regex = 0;
+    tmp.d_subExpression = 0;
+
+    return *this;    
+}
