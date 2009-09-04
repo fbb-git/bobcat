5c448502df75bc75d561498d35047df2818a37fc
Frank B. Brokken <f.b.brokken@rug.nl>
2009-09-04 21:46:48 +0000

    size_t * to unsigned int * repair -> 2.04.01
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@511 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/hmacbuf/close.cc
+++ bobcat/hmacbuf/close.cc
@@ -3,13 +3,13 @@
 void HMacBuf::close()
 {
     char *digestbuf = new char[EVP_MAX_MD_SIZE];
-    size_t digestbufLen;
 
     if (pptr() > pbase())
        HMAC_Update(&d_pimpl->ctx, 
                    reinterpret_cast<unsigned char *>(d_pimpl->buffer), 
                    pptr() - pbase());
          
+    unsigned int digestbufLen;
     HMAC_Final(&d_pimpl->ctx, reinterpret_cast<unsigned char *>(digestbuf), 
                &digestbufLen);
 
