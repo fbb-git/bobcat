d62c6960a3ed206d547b6f1264f6a934d09d2155
Frank B. Brokken <f.b.brokken@rug.nl>
2009-09-23 10:14:32 +0000

    Removed leading zeroes from textual BigInt values
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@519 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/bigint/insertinto.cc
+++ bobcat/bigint/insertinto.cc
@@ -10,7 +10,21 @@ ostream &BigInt::insertInto(ostream &out) const
                 BN_bn2hex
         )(&d_bn);
 
-    out << cp;
+    bool isNegative = (*cp == '-');
+    
+    char *skipZeroes = cp + isNegative;
+
+    while (*skipZeroes == '0')              // cut off leading zeros
+        ++skipZeroes;
+
+    if (cp + isNegative + 1 == skipZeroes)  // if just 1 char: keep it.
+        --skipZeroes;
+
+    if (isNegative)
+        *--skipZeroes = '-';                // restore the '-'
+
+    out << skipZeroes;                      // insert the value
+
     OPENSSL_free(cp);
 
     return out;
