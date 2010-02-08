466ada549a386f44f58318c2c1bf0201054d0f25
Frank B. Brokken <f.b.brokken@rug.nl>
2010-02-08 15:08:32 +0000

    Repaired Bug#564967: bobcat: ftbfs with gcc-4.5
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@544 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/cgi/push.cc
+++ bobcat/cgi/push.cc
@@ -3,8 +3,10 @@
 void CGIFSA::push()
 {
     for_each(d_buffer.begin(), d_buffer.end(), d_stack, 
+#if __GNUC__ > 4 && __GNUC_MINOR__ >= 4
+#then
         static_cast<void (stack<char>::*)(char &&)>(&stack<char>::push));
-
-// With g++4-4 (?)
-//        static_cast<void (stack<char>::*)(char const &)>(&stack<char>::push));
+#else
+        static_cast<void (stack<char>::*)(char const &)>(&stack<char>::push));
+#endif
 }
