cf135cb8a384ff53fb77a223d7cfec0d420a0531
Frank B. Brokken <f.b.brokken@rug.nl>
2010-02-09 14:56:07 +0000

    Moved cond. compiler info to changelog, out of cgi/
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@545 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/cgi/accept.cc
+++ bobcat/cgi/accept.cc
@@ -2,18 +2,9 @@
 
 void CGIFSA::accept()
 {
-    for_each(d_buffer.rbegin() + 1, d_buffer.rend(), 
-            d_stack, 
-
-#if __GNUC__ > 4 && __GNUC_MINOR__ >= 4
-#then
-// With g++4-4 (?)
-        static_cast<void (stack<char>::*)(char &&)>(&stack<char>::push));
-#else
-        static_cast<void (stack<char>::*)(char const &)>(&stack<char>::push));
-#endif
+    for_each(d_buffer.rbegin() + 1, d_buffer.rend(), d_stack, 
+             static_cast<void (stack<char>::*)(char &&)>(&stack<char>::push));
 
     acceptAll();
-
     d_stack.push(*d_buffer.rbegin());
 }
--- bobcat/cgi/push.cc
+++ bobcat/cgi/push.cc
@@ -3,10 +3,5 @@
 void CGIFSA::push()
 {
     for_each(d_buffer.begin(), d_buffer.end(), d_stack, 
-#if __GNUC__ > 4 && __GNUC_MINOR__ >= 4
-#then
         static_cast<void (stack<char>::*)(char &&)>(&stack<char>::push));
-#else
-        static_cast<void (stack<char>::*)(char const &)>(&stack<char>::push));
-#endif
 }
