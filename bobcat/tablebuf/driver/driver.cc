6679e2109462fa40a2d79b801497064c7bc4b548
Frank B. Brokken <f.b.brokken@rug.nl>
2010-04-30 20:18:17 +0000

    Added driver for TableBuf
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@583 95c995b8-3b1c-0410-acf1-adacd5d40d98

---
+++ bobcat/tablebuf/driver/driver.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+
+#include <bobcat/tablebuf>
+#include <bobcat/tablelines>
+
+using namespace std;
+using namespace FBB;
+
+int main()
+{
+    TableLines tablelines;
+                                // width/separators of cols 0, 1 and 2
+    tablelines << 0 << " | " << " | ";
+
+                                // hline over cols 1 and 2 of row 1
+    tablelines << TableLines::HLine(1, 1, 3); 
+
+    TableBuf tab(tablelines, 3, TableBuf::ROWWISE, TableBuf::EQUALWIDTH);
+
+    tab << Align(0, std::left);     // set column non-default alignment
+
+    ostream out(&tab);
+
+    out << "one" << fs << fs << "two" << fs << 
+            fs << "three";
+
+    cout << tab << '\n';            // complete the table and insert
+}
+
