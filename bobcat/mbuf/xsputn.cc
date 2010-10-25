6715948a854a42700048ebc594fdd9df22b800f7
Frank B. Brokken <f.b.brokken@rug.nl>
2010-10-25 15:57:39 +0000

    WIP
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@641 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/mbuf/xsputn.cc
+++ bobcat/mbuf/xsputn.cc
@@ -2,7 +2,7 @@
 
 std::streamsize Mbuf::xsputn(char const *buf, std::streamsize n)
 {
-    atNewline();
+    atFirstChar();
     d_ostr.write(buf, n);
     return n;               // only if the write succeeded
 }
