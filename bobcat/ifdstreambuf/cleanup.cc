d6db964e965491f76dae0c0a71b3c02a01cb648a
Frank B. Brokken <f.b.brokken@rug.nl>
2011-01-08 15:21:15 +0000

    WIP on [IO]FdStreambuf classes
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@693 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/ifdstreambuf/cleanup.cc
+++ bobcat/ifdstreambuf/cleanup.cc
@@ -2,9 +2,12 @@
 
 void IFdStreambuf::cleanup(Mode mode)
 {
-    if (mode == CLOSE_FD)
-        close(d_fd);
-
     if (d_buffer)
+    {
+        if (mode == CLOSE_FD)
+            close(d_fd);
+
         delete [] d_buffer;
+        d_buffer = 0;
+    }
 }
--- bobcat/ofdstreambuf/cleanup.cc
+++ bobcat/ofdstreambuf/cleanup.cc
@@ -8,6 +8,7 @@ void OFdStreambuf::cleanup(Mode mode)
         if (d_mode == CLOSE_FD)
             close(d_fd);
         delete [] d_buffer;
+        d_buffer = 0;
     }
 }
     
