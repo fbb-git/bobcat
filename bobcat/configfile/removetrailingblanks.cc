ce396432c9f436d07e02adbe722f0daf03aab19e
Frank B. Brokken <f.b.brokken@rug.nl>
2011-02-04 14:11:50 +0000

    Repaired bug detected by Kees Visser in nextline.cc
    git-svn-id: https://bobcat.svn.sourceforge.net/svnroot/bobcat/trunk@714 95c995b8-3b1c-0410-acf1-adacd5d40d98

--- bobcat/configfile/removetrailingblanks.cc
+++ bobcat/configfile/removetrailingblanks.cc
@@ -3,5 +3,7 @@
                                                 // `line' must have content.
 void ConfigFile::removeTrailingBlanks(string &line)
 {
-    line = line.substr(0, line.find_last_not_of(" \t") + 1);
+                            // convert idx of last non-blank char to 
+                            // a length, resize to that length.
+    line.resize(1 + line.find_last_not_of(" \t"));
 }   
