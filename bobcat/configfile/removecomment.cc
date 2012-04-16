6ed93f885a81431d99bd69ea9342a487d1b71ddb
Frank B. Brokken <fbb@users.sourceforge.net>
2012-04-16 11:52:57 +0200

    Added recognition of \# as intentional #-character to ConfigFile

--- bobcat/configfile/removecomment.cc
+++ bobcat/configfile/removecomment.cc
@@ -4,6 +4,14 @@ void ConfigFile::removeComment(string &line)
 {
     size_t pos;
 
-    if ((pos = line.find_first_of('#')) != string::npos)
+    if 
+    (                               // saw #
+        (pos = line.find_first_of('#')) != string::npos
+        &&                          // but not \#
+        pos > 0 && line[pos - 1] != '\\'
+    )
         line.erase(pos);            // throw away beyond '#'
+
+    while ((pos = line.find("\\#")) != string::npos)    // replace \# by #
+        line.replace(pos, 2, "#");
 }   
