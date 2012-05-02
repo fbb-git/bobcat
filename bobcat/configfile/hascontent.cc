a0f0ab4ad68cc547fc08903086dc8701552bd1d2
Frank B. Brokken <f.b.brokken@rug.nl>
2012-05-02 20:36:02 +0200

    removed SF files

--- bobcat/configfile/hascontent.cc
+++
@@ -1,6 +0,0 @@
-//#include "configfile.ih"
-//
-//bool ConfigFile__::hasContent(string const &line)
-//{
-//    return (line.find_first_not_of(" \t") != string::npos);
-//}   
--- bobcat/configfile/nextline.cc
+++ bobcat/configfile/nextline.cc
@@ -22,47 +22,4 @@ bool ConfigFile__::nextLine(istream &inStream, string &dest)
 
     return dest.length();
 }
-//
-//
-//        bool literalBackslash = false;              // last char is literal
-//                                                    // backslash?
-//        if (d_rmComment)
-//            literalBackslash = removeComment(line);
-//                                                    // process lines ending
-//                                                    // in a backslash
-//        n_continuations = 0;
-//        if (not literalBackslash)
-//        {
-//            while (size_t lastIdx = line.length())  // as long as there are
-//            {                                       // characters
-//    
-//                --lastIdx;                          // set lastIdx to idx of 
-//                                                    // last ch.
-//    
-//                if (line[lastIdx] != '\\')          // last not \ ?
-//                    break;                          // then done here
-//                              
-//                                                    // got line continuation...
-//
-//                line.resize(lastIdx);               // erase the last char
-//
-//                                                    // append the next line
-//                n_continuations += append_next(inStream, line);
-//            }
-//        }
-//
-//        if (hasContent(line))                       // any contents on this?
-//        {
-//            removeTrailingBlanks(line);
-//
-//            if (d_indices)
-//                d_index.push_back(d_rawIndex - n_continuations - 1);
-//
-//            return true;
-//        }
-//    }
-//
-//    line.erase();
-//    return false;                       // no more lines
-//}   
 
