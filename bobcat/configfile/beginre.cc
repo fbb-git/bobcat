d9087457ad5992d56c2dd011ce384815dd56df97
Frank B. Brokken <f.b.brokken@rug.nl>
2012-05-08 10:01:37 +0200

    Cosmetic split of configile/beginre.cc into two sources

--- bobcat/configfile/beginre.cc
+++ bobcat/configfile/beginre.cc
@@ -7,12 +7,3 @@ ConfigFile__::const_RE_iterator ConfigFile__::beginRE(string const &re)
 
     return RE_iterator(d_vsIter, 0);
 }
-
-ConfigFile::const_RE_iterator ConfigFile::beginRE(string const &re)
-{
-    static bool called = false;
-    deprecated__(called, "ConfigFile::beginRE(std::string const &)");
-
-    return d_ptr->beginRE(re);
-}
-
