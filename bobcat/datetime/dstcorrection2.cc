ba2f3b3717378288c037d424148972a0e4404774
Frank B. Brokken <f.b.brokken@rug.nl>
2012-05-14 11:44:47 +0200

    Repaired DayTime bug

--- bobcat/datetime/dstcorrection2.cc
+++ bobcat/datetime/dstcorrection2.cc
@@ -3,9 +3,11 @@
 int DateTime::dstCorrection(bool *ok) const
 {
     TimeStruct ts;
+
+    tzset();                // see the localtime_r manpage: tzset() required
     TimeStruct *ret =  localtime_r(&d_utcSec, &ts);
 
-    if (ret == 0)    // 0: can't compute
+    if (not (*ok = ret))    // 0: can't compute
         return 0;
 
     return ts.tm_isdst == 1 ? 3600 : 0;
