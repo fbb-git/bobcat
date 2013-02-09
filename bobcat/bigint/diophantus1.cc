a9741345b663becf5246bb6e93cd3d877868f63c
Frank B. Brokken <f.b.brokken@rug.nl>
2013-02-09 12:40:52 +0100

    Added BigInt::diophantus members

---
+++ bobcat/bigint/diophantus1.cc
@@ -0,0 +1,17 @@
+#include "bigint.ih"
+
+long long BigInt::diophantus(long long *factor1, long long *factor2, 
+                             long long const &value1, long long const &value2)
+{
+    if (long long modulo = value1 % value2)
+    {
+        long long nestedFactor1;
+        long long gcd = diophantus(&nestedFactor1, factor2, value2, modulo);
+        *factor1 = *factor2;
+        *factor2 = nestedFactor1 - (value1 / value2) * *factor2;
+        return gcd;
+    }
+    *factor1 = 0;
+    *factor2 = 1;
+    return value2;
+}
