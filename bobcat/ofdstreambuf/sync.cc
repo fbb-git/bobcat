672c308fea68b2477d1271f99f78760860c75eeb
Frank B. Brokken <f.b.brokken@rug.nl>
2013-02-24 13:12:39 +0100

    Updated docs and ofdstreambuf/fork files to remove warnings

--- bobcat/ofdstreambuf/sync.cc
+++ bobcat/ofdstreambuf/sync.cc
@@ -4,7 +4,10 @@ int OFdStreambuf::sync()
 {
     if (pptr() > pbase())
     {
-        write(d_fd, d_buffer, pptr() - pbase());    
+        if (write(d_fd, d_buffer, pptr() - pbase()) < 0)
+            std::cerr << "[Warning] OFdStreambuf::sync could not "
+                            "write to FD " << d_fd << '\n';
+
         setp(d_buffer, d_buffer + d_n);             
     }
     return 0;
