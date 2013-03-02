3cd51cca16882d5ae0f4ff88eaf9bb5d0fe64e71
tony mancill <tmancill@debian.org>
2013-03-01 22:18:31 -0800

    Imported Upstream version 1.17.1

---
+++ datetime/rfc2822.cc
@@ -0,0 +1,13 @@
+#include "datetime.ih"
+
+string DateTime::rfc2822() const
+{
+    ostringstream out;
+    out << *this << ' ' << 
+            setfill('0') << showpos << 
+                setw(3) << internal << -d_timezone / 3600 << 
+            noshowpos << 
+                setw(2) << -d_timezone % 3600 / 60;
+
+    return out.str();
+}
