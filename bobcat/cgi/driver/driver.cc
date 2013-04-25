9f244267ccfd6373c1d3e5a2596c70842ed31358
Frank B. Brokken <f.b.brokken@rug.nl>
2013-04-25 16:42:35 +0200

    Added CGI::param1, updated CGI documentation

--- bobcat/cgi/driver/driver.cc
+++ bobcat/cgi/driver/driver.cc
@@ -1,19 +1,28 @@
 #include "main.ih"
 
-void outstr(string const &str)
-{
-    cout << CGI::dos2unix(str) << "\n"
-            "    ";
-}
-
 void showParam(CGI::MapStringVector::value_type const &mapValue)
 {
-    cout << "Param: " << mapValue.first << "\n"
+    cout << "Param: " << mapValue.first << '\n';
+
+    for (auto &str: mapValue.second)
+        cout << "    " << CGI::dos2unix(str) << "\n"
             "    ";
 
-    FBB::for_each(mapValue.second.begin(), mapValue.second.end(), outstr);
-    cout << endl;
+    cout << '\n';
 }
+
+std::string param1(CGI &cgi, std::string const &variable)
+{
+    auto &vect = cgi.param(variable);
+
+    string ret;
+
+    if (not vect.empty())
+        ret = vect[0];
+
+    return ret;
+}
+
 
 int main(int argc, char **argv)
 try
@@ -27,46 +36,50 @@ try
     while (getline(in, line))
     {
         size_t pos = line.find('=');
+
         if (pos == string::npos)
             continue;
-        if (setenv(line.substr(0, pos).c_str(), 
+                            // set environment vars simulating
+                            // a GET form
+        if (setenv(line.substr(0, pos).c_str(),     
                line.substr(pos + 1).c_str(), true) == 0)
         {
             if (arg.option('e'))
                 cout << line.substr(0, pos).c_str() << '=' <<
-                       line.substr(pos + 1).c_str() << endl;
+                       line.substr(pos + 1).c_str() << '\n';
         }
         else
-            cout << "FAILED: setenv " << line << endl;
+            cout << "FAILED: setenv " << line << '\n';
     }
 
-    CGI cgi(false);             // by default no escapes
+    CGI cgi(false);             // chars are not escaped
 
     cgi << arg[1];
 
     if (arg.option(&line, 'm'))
         cgi.setMaxUploadSize(A2x(line), *line.rbegin());
 
-    cout << "Max upload size (b): " << cgi.maxUploadSize() << endl;
+    cout << "Max upload size (b): " << cgi.maxUploadSize() << '\n';
 
     CGI::Method method = cgi.method();
 
     cout << "To escape:\n" << 
             cgi << "\n"
-            "Method: " << (method == CGI::GET ? "GET" : "POST") <<
-            endl;
+            "Method: " << (method == CGI::GET ? "GET" : "POST") << 
+            '\n';
 
-    cout << "Query string: " << cgi.query() << endl;
+    cout << "Query string: " << cgi.query() << '\n';
 
-    cgi.param("submit");
+    cout << "Submit string: `" << param1(cgi, "submit") << "'\n";
 
-    FBB::for_each(cgi.begin(), cgi.end(), &showParam);
+    for (auto &mapElement: cgi)
+        showParam(mapElement);
 
     cout << "END OF PROGRAM\n";
 }
-catch (Errno const &err)
+catch (exception const &err)
 {
-    cout << err.why() << endl;
+    cout << err.what() << '\n';
     return 1;
 }
 catch (...)
