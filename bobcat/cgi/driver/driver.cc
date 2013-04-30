1b4e8820953eb285a064ba419bc3dfc970b0d5ae
Frank B. Brokken <f.b.brokken@rug.nl>
2013-04-30 21:06:45 +0200

    Added CGI::param1 and updated CGI-manpage

--- bobcat/cgi/driver/driver.cc
+++ bobcat/cgi/driver/driver.cc
@@ -11,25 +11,13 @@ void showParam(CGI::MapStringVector::value_type const &mapValue)
     cout << '\n';
 }
 
-std::string param1(CGI &cgi, std::string const &variable)
-{
-    auto &vect = cgi.param(variable);
-
-    string ret;
-
-    if (not vect.empty())
-        ret = vect[0];
-
-    return ret;
-}
-
-
 int main(int argc, char **argv)
 try
 {
     Arg &arg = Arg::initialize("evhm:", argc, argv);
 
-    arg.versionHelp(usage, version, 2);
+    // usage and version are in the source archive in .../cgi/driver
+    // arg.versionHelp(usage, version, 2);
 
     ifstream in(arg[0]);
     string line;
@@ -70,7 +58,7 @@ try
 
     cout << "Query string: " << cgi.query() << '\n';
 
-    cout << "Submit string: `" << param1(cgi, "submit") << "'\n";
+    cout << "Submit string: `" << cgi.param1("submit") << "'\n";
 
     for (auto &mapElement: cgi)
         showParam(mapElement);
--- bobcat/cgi/driver/usage.cc
+++ bobcat/cgi/driver/usage.cc
@@ -13,18 +13,20 @@ void usage(std::string  const  &progname)
     progname << " by " << Icmbuild::author << "\n" <<
     progname << " V" << Icmbuild::version << " " << Icmbuild::year << "\n"
     "\n"
-    "Usage: " << progname << " [options] args < arg.cin\n"
+    "Usage: " << progname << " [options] arg to-escape < arg.cin\n"
     "Where:\n"
     "   [options] - optional arguments (short options between parentheses):\n"
     "      -e               - show the environment variables that were set\n"
     "      -m<size>[bkmg]   - max uploadsize to use\n"
     "      -h               - provide this help\n"
     "      -v               - show version information and terminate\n"
-    "   args                - 1st arg: file defining envvars\n"
-    "                         (see `get' and `post', e.g. post1)\n"
-    "                         2nd args: RE defining characters to escape.\n"
-    "                         (e.g., '\\\\')\n"
+    "   arg                 - file defining envvars\n"
+    "                         (see, e.g.,  `get' and `post1')\n"
+    "   to-escape           - RE defining characters to escape.\n"
+    "                         (e.g., '[:cgi:]')\n"
     "   < arg.cin           - file to read, simulating form-output\n"
     "                         (e.g., post1.cin)\n" 
+    "   For the 'get' method the input redirection is not used/required\n"
     "\n";
 }
+
