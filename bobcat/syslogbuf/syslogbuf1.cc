ba1903f1f8469729ed08a9e13e591b3edfddaf90
Frank B. Brokken <f.b.brokken@rug.nl>
2013-05-09 12:01:04 +0200

    Initializing syslogbuf1.cc's d_data to 0

--- bobcat/syslogbuf/syslogbuf1.cc
+++ bobcat/syslogbuf/syslogbuf1.cc
@@ -4,7 +4,8 @@ Syslogbuf::Syslogbuf(std::string const &ident,
                      Priority priority, Facility facility, int option)
 :
     d_priority(priority),
-    d_orgPriority(priority)
+    d_orgPriority(priority),
+    d_data(0)
 {
     reset(ident, facility, option);
 }
