399494e6d9ea989f6e9b3223bc6b652d36d3ef6b
Frank B. Brokken <f.b.brokken@rug.nl>
2013-08-16 18:17:12 +0200

    Added class IBase64Stream

--- bobcat/base64streambufbase/base64streambufbase1.cc
+++ bobcat/base64streambufbase/base64streambufbase1.cc
@@ -1,6 +1,7 @@
 #include "base64streambufbase.ih"
 
-Base64StreambufBase::Base64StreambufBase(std::istream &in)
-:
+Base64StreambufBase::Base64StreambufBase(std::istream &in, size_t bufSize)
+:   
+    IFilterStreambuf(bufSize),
     d_in(in)
 {}
