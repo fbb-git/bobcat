c425c20eca0592f5aa2443adc6ea56a5fbc6a27f
Frank B. Brokken <f.b.brokken@rug.nl>
2013-10-02 16:12:41 +0200

    Added .f files to binops

---
+++ bobcat/binops/opadd2.f
@@ -0,0 +1,8 @@
+template <typename LHS, typename RHS>
+inline typename FBB::Use<LHS, RHS>::type operator+(LHS const &lhs, 
+                                                   RHS const &rhs)
+{
+    typename FBB::Use<LHS, RHS>::type tmp(lhs);
+    tmp += rhs;
+    return tmp;
+}
