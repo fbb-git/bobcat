e26bd2237ba6ec0bd46bc657f56b15ba10e3667b
Frank B. Brokken <f.b.brokken@rug.nl>
2013-12-22 23:20:12 +0100

    Updated signal, pipe and redirector man-pages, added member remove to Signal

---
+++ bobcat/signal/remove.cc
@@ -0,0 +1,21 @@
+#include "signal.ih"
+
+void Signal::remove(size_t signum, SignalHandler &object)
+{
+    verify(signum, "Signal::remove");
+
+    if (d_signals.size() >= signum)
+        throw Exception(1) << "no signal handlers for signal " << signum <<
+                              " were installed";
+
+    auto iter = 
+        find(d_signals[signum].begin(), d_signals[signum].end(), &object); 
+
+    if (iter == d_signals[signum].end())
+        throw Exception(1) << "object " << &object << 
+                " did not register a handler for signal " << signum;
+
+    d_signals[signum].erase(iter);
+
+    signal(signum, handler); 
+}
