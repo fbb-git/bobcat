7e79e4d13cec486171ccab747c4e1fa4ebc31c13
Frank B. Brokken <f.b.brokken@rug.nl>
2014-03-04 10:13:42 +0100

    Imported Upstream version 3.22.01

--- signal/verify.cc
+++ signal/verify.cc
@@ -1,5 +1,12 @@
 #include "signal.ih"
 
+// This is required for the hurd-i386 arch, which apparently 
+// fails to implement the POSIX standard:
+
+#ifndef SIGRTMAX
+    #define SIGRTMAX _NSIG
+#endif        
+
 void Signal::verify(size_t signum, char const *member)
 {
     if (signum > static_cast<size_t>(SIGRTMAX))
