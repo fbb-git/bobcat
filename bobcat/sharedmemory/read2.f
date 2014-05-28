fa04169c0431157437b9165e9f426917dcec8dd2
Frank B. Brokken <f.b.brokken@rug.nl>
2014-05-28 18:45:49 +0200

    Refining the SharedMemory man-page and some members

--- bobcat/sharedmemory/read2.f
+++ bobcat/sharedmemory/read2.f
@@ -1,9 +1,10 @@
 template <typename Type>
-int SharedMemory::read(std::ios::off_type offset, Type *value, 
+int SharedMemory::read(Type *value, std::ios::off_type offset, 
                         std::ios::seekdir origin) 
 {
     if (seek(offset, origin) == -1)
-        throw Exception() << "SharedMemory::read: failed to seek " << offset;
+        throw Exception() << "SharedMemory::read: seek to " << offset <<
+                                                                "failed";
                             
     return read(value);
 }
--- bobcat/sharedmemory/write2.f
+++ bobcat/sharedmemory/write2.f
@@ -2,6 +2,8 @@ template <typename Type>
 int SharedMemory::write(Type const &value, std::ios::off_type offset,
                         std::ios::seekdir origin) 
 {
-    seek(offset, origin);
+    if (seek(offset, origin) == -1)
+        throw Exception() << "SharedMemory::write: seek to " << offset <<
+                                                                "failed";
     return write(value);
 }
