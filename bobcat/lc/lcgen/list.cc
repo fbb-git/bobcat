5be48d9002018deb03b1cd8faf9fac17562bd133
Frank B. Brokken <f.b.brokken@rug.nl>
2015-09-18 20:04:56 +0200

    removed lc/, fixed 'build libraries'

--- bobcat/lc/lcgen/list.cc
+++
@@ -1,9 +0,0 @@
-#include "main.hh"
-
-void list(ostream &out, size_t nTs, char const *txt)
-{
-    for (size_t idx = 1; idx < nTs; ++idx)
-        out << txt << idx << ", ";
-
-    out << txt << nTs;
-}
--- bobcat/lc/lcgen/typedeflist.cc
+++
@@ -1,9 +0,0 @@
-#include "main.hh"
-
-void typeDefList(ostream &out, size_t nTs)
-{
-    out << '<';
-    list(out, nTs, "typename T");
-    out << '>';
-}
-
