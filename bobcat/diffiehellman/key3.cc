207aa80ad4590a9c636c166e43ba0202827ec3e1
Frank B. Brokken <f.b.brokken@rug.nl>
2016-08-01 08:11:25 +0200

    updated DiffieHellman's key members to OpenSSL1.1.0

--- bobcat/diffiehellman/key3.cc
+++ bobcat/diffiehellman/key3.cc
@@ -5,11 +5,13 @@ string DiffieHellman::key(istream &peerPublicStream)
     skip(peerPublicStream, 2);
     
     BIGNUM *otherPubKey;
+
     if (not read(peerPublicStream, &otherPubKey))
-        throw Exception() << s_header << 
+        throw Exception{} << s_header << 
                                 "could not read the peer's public key";
         
-    d_otherPubKey.reset(otherPubKey);
+    BN_free(d_otherPubKey);
+    d_otherPubKey = otherPubKey;
     
     return key();
 }
