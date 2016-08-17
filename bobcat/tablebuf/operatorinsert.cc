ec358733b07fb1a8018602a46e92e7511ba5a97f
Frank B. Brokken <f.b.brokken@rug.nl>
2016-08-17 10:32:41 +0200

    Ready for 4.03.00

--- bobcat/tablebuf/operatorinsert.cc
+++ bobcat/tablebuf/operatorinsert.cc
@@ -1,9 +1,13 @@
 #include "tablebuf.ih"
 
+namespace FBB
+{
                         // displays the table
-std::ostream &FBB::operator<<(std::ostream &out, TableBuf &table)
+std::ostream &operator<<(std::ostream &out, TableBuf &table)
 {
     table.sync();
     TableBase &tb = table;
     return out << tb;
 }
+
+} // FBB
